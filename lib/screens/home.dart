a22e47432476162874334e247331fb18c329e4a5
Asasaafi <annisafitriana04@gmail.com>
2026-04-14 15:32:41 +0700

    donee

---
+++ lib/screens/home.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'login.dart';
+import '../homepage.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  void logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacementNamed(context, 'login');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Notes App'),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  onPressed: () => logout(context),
+                  icon: const Icon(Icons.logout),
+                )
+              ],
+            ),
+            body: const HomePage(),
+          );
+        } else {
+          return const LoginScreen();
+        }
+      },
+    );
+  }
+}
